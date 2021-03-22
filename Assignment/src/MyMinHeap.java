40f37f2d7ed27ab2e2616b4b6b282e81380a7ff8
chiefjazzdiewltr <zacharygillions@gmail.com>
2021-03-22 21:04:57 +1300

    Generic Min Heap

--- Assignment/src/MyMinHeap.java
+++ Assignment/src/MyMinHeap.java
@@ -1,7 +1,17 @@
 import java.io.*;
 
 public class MyMinHeap {
-    public MyMinHeap(int heapSize) {
+
+    public MyMinHeap(String heapSize) {
+        try {
+            int s = Integer.parseInt(heapSize);
+        }
+        catch (NumberFormatException nfe) {
+            int s = 32;
+        }
+    }
+
+    public <T> void insert(T item) {
 
     }
 }
